b4be1f3fe51a890df6b45cb4c8adc0caae08ec79
shawn <shawn24601@gmail.com>
2017-01-03 10:42:23 +0800

    new

--- publish.sh
+++ publish.sh
@@ -1,3 +1,4 @@
 tar -vzcf src.tar.gz ./src
-scp ./src.tar.gz textMining:~/pubmedTextMining
-ssh textMining "tar  -C ~/pubmedTextMining -vzxf pubmedTextMining/src.tar.gz"
+scp ./src.tar.gz textMining:~/PubmedExtractor
+echo "start decompress"
+ssh textMining "tar  -C ~/PubmedExtractor -vzxf ~/PubmedExtractor/src.tar.gz"
